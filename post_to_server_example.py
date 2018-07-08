8c3bb09668b0d799aa76f4360959c1d84b8155e9
GL <ZXGAOLING@GMAIL.com>
2018-07-08 19:58:37 +0800

    测试了,flask的数据传输与页面展示等功能,运行良好

---
+++ post_to_server_example.py
@@ -0,0 +1,32 @@
+import json
+import request
+
+posts=[
+            {'key':'test1',
+             'value':10
+             },
+             {'key':'test2',
+             'value':11
+             },
+             {'key':'test3',
+             'value':12
+             },
+             {'key':'test4',
+             'value':13
+             },
+             {'key':'test5',
+             'value':14
+             },
+             {'key':'test6',
+             'value':15
+             },
+             {'key':'test6',
+             'value':15
+             }
+           ]
+
+json_data=json.dumps(posts)
+
+request.post('http://159.89.155.162:5000/info',data=json_data)
+
+
--- server.py
+++ server.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 r_ip='127.0.0.1'
 r_port='10000'
 
+
+posts=[
+            {'key':'温度',
+             'value':12
+             },
+             {'key':'湿度',
+             'value':12
+             },
+             {'key':'氧气',
+             'value':12
+             },
+             {'key':'阳光',
+             'value':12
+             },
+             {'key':'声音',
+             'value':12
+             }
+           ]
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -17,10 +36,21 @@ def set_r_ip():
     return jsonify(ip=user_ip,port=user_port)    
 
 
-@app.route('/info')
+@app.route('/info',methods=['GET'])
 def show_info():
-    a_dict={"key1":10,"key2":20}
-    return render_template('info.html',my_dict=a_dict)
+    
+    return render_template('info.html',posts=posts)
+
+@app.route('/info',methods=['POST'])
+def post_info():
+    global posts
+    json_data=request.get_json(force=True)
+    print(json_data)
+    print(json_data[0])
+    print(type(json_data))
+    print(type(json_data[0]))
+    posts=json_data
+    return 'ok'
 
 if __name__=='__main__':
     app.run(debug=True)
